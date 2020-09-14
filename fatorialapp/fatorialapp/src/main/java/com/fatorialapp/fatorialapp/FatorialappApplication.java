8b4d91bd84aed47c1f3215754035a7dc6b1bb2cf
Jefferson <jeff.junior98@gmail.com>
2020-09-14 16:50:41 -0300

    inicializacao da api com spring boot

---
+++ fatorialapp/fatorialapp/src/main/java/com/fatorialapp/fatorialapp/FatorialappApplication.java
@@ -0,0 +1,13 @@
+package com.fatorialapp.fatorialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatorialappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatorialappApplication.class, args);
+	}
+
+}
---
+++ fatorialapp/fatorialapp/src/test/java/com/fatorialapp/fatorialapp/FatorialappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatorialapp.fatorialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatorialappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
